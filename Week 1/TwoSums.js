7d3b21ea0e9c2b9446071c7f32fdb3be50e43846
Yash Shelar <87018353+YashShelar007@users.noreply.github.com>
2022-12-24 18:16:47 -0700

    Added solution description

--- Week 1/TwoSums.js
+++ Week 1/TwoSums.js
@@ -1,7 +1,7 @@
 // Two Sum
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-// my initial nested for loop solution O(n^2)
 
+// my initial nested for loop solution O(n^2)
 var twoSum = function(nums, target) {
     
     var resultArr = [];
