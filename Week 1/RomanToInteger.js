c3c522faa9cd678c35d723eb165d997544cde998
Yash Shelar <87018353+YashShelar007@users.noreply.github.com>
2022-12-24 18:27:29 -0700

    Added improved solution

--- Week 1/RomanToInteger.js
+++ Week 1/RomanToInteger.js
@@ -39,3 +39,27 @@ var romanToInt = function(s) {
     }
     return sum;
 };
+
+// My improved hashmap solution O(n)
+var romanToInt = function(s) {
+    var sum = 0;
+    var roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+    for( let i = 0; i < s.length; i++) {
+        if(roman[s[i]] < roman[s[i + 1]]) {
+            sum = sum - roman[s[i]];
+        }
+        else {
+            sum = sum + roman[s[i]];
+        }
+    }
+    return sum;
+};
